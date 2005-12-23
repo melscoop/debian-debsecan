744139a79dbb14f905e9da11775ed77386b2d9a1
Florian Weimer <fw@deneb.enyo.de>
2005-12-23 14:33:23 +0000

    Testsuite: print error message when debsecan exists with error

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -10,16 +10,21 @@ for testcase in [0-9][0-9][0-9] ; do
     for format in summary packages bugs detail report ; do
 	for suite in sid ; do
 	    if test -e $testcase/$suite ; then
-		python ../src/debsecan --suite $suite \
+		if python ../src/debsecan --suite $suite \
 		    --source "$url/$testcase" \
 		    --history $testcase/history \
 		    --status $testcase/status \
-		    --format $format > $testcase/out.$format 2>&1
-		if test $format = summary ; then
-		    sort $testcase/out.$format > $testcase/out.$format.1
-		    mv $testcase/out.$format.1 $testcase/out.$format
-		fi
-		diff -u $testcase/exp.$format $testcase/out.$format
+		    --format $format > $testcase/out.$format 2>&1 ; then
+		    if test $format = summary ; then
+			sort $testcase/out.$format > $testcase/out.$format.1
+			mv $testcase/out.$format.1 $testcase/out.$format
+		    fi
+		    diff -u $testcase/exp.$format $testcase/out.$format
+		else
+		    echo "FAIL: debsecan failed.  Output follows:"
+		    cat $testcase/out.$format
+		    exit 1
+	        fi
 	    fi
 	done
     done
